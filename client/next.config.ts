182d9a22a373d49ada0b49831b63be98241dd087
Rustom Yadav <vijayyadav.official@outlook.com>
2026-09-22 21:49:06 +0530

    chore: add setup script, Next.js configuration

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   // Enables standalone output for Docker (smaller image size)
   output: 'standalone',
 
   reactCompiler: true,
+
+  outputFileTracingRoot: path.join(__dirname),
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
